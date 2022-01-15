f83b55cb03072bcc3425c55046cedcfb62585a44
NikitaGoroshko7 <nikita.goroshko7@gmail.com>
2022-01-15 23:48:21 +0300

    Окно регистрации и авторизации https://trello.com/c/3DfBOrjm/9-%D0%BE%D0%BA%D0%BD%D0%BE-%D1%80%D0%B5%D0%B3%D0%B8%D1%81%D1%82%D1%80%D0%B0%D1%86%D0%B8%D0%B8-%D0%B8-%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D0%B8 Added AccountController and Views Login and Register. Added style for Views

---
+++ CrowdFundingApplication/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrowdFundingApplication.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register() => View();
+
+        [HttpGet]
+        public IActionResult Login() => View();
+    }
+}
